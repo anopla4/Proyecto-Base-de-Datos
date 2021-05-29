045c6305e55ae07ea2db16886837f8aba68eed17
Ana Paula <ana.arguelles@estudiantes.matcom.uh.cu>
2021-05-29 11:21:13 -0700

    feature: Fill credentials of user

--- sb_frontend/src/components/Login/Login.jsx
+++ sb_frontend/src/components/Login/Login.jsx
@@ -1,9 +1,27 @@
 import React, { Component } from "react";
-
+import { Form, Button } from "react-bootstrap";
 class Login extends Component {
   state = {};
   render() {
-    return <p>ana</p>;
+    return (
+      <Form>
+        <Form.Group controlId="email">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Ponga el correo" />
+        </Form.Group>
+
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Contraseña" />
+        </Form.Group>
+        {/* <Form.Group controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group> */}
+        <Button variant="primary" type="submit">
+          Aceptar
+        </Button>
+      </Form>
+    );
   }
 }
 
