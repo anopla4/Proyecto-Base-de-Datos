07ee7da2141b6efd04d447c8fd619573565b0a2f
Ana Paula <ana.arguelles@estudiantes.matcom.uh.cu>
2021-06-01 16:35:21 -0700

    feat: Add Login component

--- sb_frontend/src/components/Login/Login.jsx
+++ sb_frontend/src/components/Login/Login.jsx
@@ -1,25 +1,68 @@
 import React, { Component } from "react";
 import { Form, Button, Col, Container } from "react-bootstrap";
 class Login extends Component {
-  state = {};
+  state = {
+    username: undefined,
+    password: undefined,
+  };
+
+  onFormSubmit = (e) => {
+    let user = {
+      username: this.state.username,
+      password: this.state.password,
+    };
+    fetch("https://localhost:44334/api/Authenticate/login", {
+      mode: "cors",
+      headers: { "Content-Type": "application/json" },
+      method: "POST",
+      body: JSON.stringify(user),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+          return response.json();
+      })
+      .then((response) => {
+        return response.token;
+      })
+      .catch(function (error) {
+        console.log("Hubo un problema con la petición Fetch:" + error.message);
+      });
+  }
+
   render() {
     return (
       <Container alignSelf="center" className="mt-5">
+        {this.state.username}
+        {this.state.password}
         <Col className="center">
-          <Form style={{ width: "50%" }}>
-            <Form.Group controlId="email">
-              <Form.Label>Correo electrónico</Form.Label>
-              <Form.Control type="email" placeholder="Ponga el correo" />
+          <Form>
+            <Form.Group controlId="username">
+              <Form.Label>Nombre de Usuario</Form.Label>
+              <Form.Control
+                type="username"
+                placeholder="Ponga el correo"
+                onChange={(e) => {this.setState({username:e.target.value})}}
+              />
             </Form.Group>
 
             <Form.Group controlId="password">
               <Form.Label>Contraseña</Form.Label>
-              <Form.Control type="password" placeholder="Contraseña" />
+              <Form.Control
+                type="password"
+                placeholder="Contraseña"
+                onChange={(e) => {this.setState({password:e.target.value})}}
+              />
             </Form.Group>
-            {/* <Form.Group controlId="formBasicCheckbox">
+            <Form.Group controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group> */}
-            <Button variant="primary" type="submit" style={{ float: "right" }}>
+        </Form.Group>
+            <Button 
+              variant="primary"
+              style={{ float: "right" }}
+              onClick={this.onFormSubmit}
+            >
               Aceptar
             </Button>
           </Form>
