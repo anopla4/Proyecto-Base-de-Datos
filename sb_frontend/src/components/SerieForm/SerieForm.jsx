d432b92b31b8abd19e1921957999f35219fb8775
Ana Paula <ana.arguelles@estudiantes.matcom.uh.cu>
2021-05-31 18:41:31 -0700

    fix: Fix serie form from control select in POST and PATCH

--- sb_frontend/src/components/SerieForm/SerieForm.jsx
+++ sb_frontend/src/components/SerieForm/SerieForm.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
-import { Form, Button, Row, Col, Container } from "react-bootstrap";
+import {
+  Form,
+  Button,
+  Row,
+  Col,
+  Container,
+  Navbar,
+  Nav,
+} from "react-bootstrap";
 import "./SerieForm.css";
 
 class SerieForm extends Component {
-  state = { edit: false, reaches: [] };
+  state = {
+    edit: false,
+    reaches: [],
+    addCaracter: false,
+  };
 
   componentWillMount() {
     fetch("https://localhost:44334/api/Caracter", { mode: "cors" })
@@ -72,6 +84,38 @@ class SerieForm extends Component {
     this.props.history.push({ pathname: "/series", state: { edited: true } });
   };
 
+  onFormSubmitCaracter = (e) => {
+    let formElements = e.target.elements;
+    const caracter_Name = formElements.name.value;
+    let caracter = {
+      caracter_Name,
+    };
+    fetch("https://localhost:44334/api/Caracter", {
+      mode: "cors",
+      headers: { "Content-Type": "application/json" },
+      method: "POST",
+      body: JSON.stringify(caracter),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function (error) {
+        console.log("Hubo un problema con la petición Fetch:" + error.message);
+      });
+    this.setState({ addCaracter: false });
+  };
+
+  handleCloseAdd = () => {
+    this.setState({ addCaracter: false });
+  };
+
+  handleAddCaracter = () => {
+    this.setState({ addCaracter: true });
+  };
+
   render() {
     const {
       id,
@@ -87,121 +131,175 @@ class SerieForm extends Component {
     return (
       <Container alignSelf="center" className="mt-4">
         <h1 className="mb-5 my-style-header">Serie</h1>
+        <Row>
+          <Col className="center">
+            <Form
+              key={
+                this.props.location.state.serie
+                  ? this.props.location.state.serie.id
+                  : 0
+              }
+              style={{ width: "70%", alignItems: "center" }}
+              onSubmit={this.onFormSubmit}
+            >
+              <Row>
+                <Col>
+                  <Form.Group style={{ width: "100%" }} controlId="name">
+                    <Form.Label>Nombre:</Form.Label>
+                    <Form.Control defaultValue={name} type="text" />
+                  </Form.Group>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Form.Group controlId="initDate" bsSize="large">
+                    <Form.Label>Fecha de incio:</Form.Label>
+                    <Form.Control
+                      defaultValue={
+                        initDate
+                          ? new Date(initDate).toISOString().substr(0, 10)
+                          : ""
+                      }
+                      disabled={this.state.edit ? true : false}
+                      type="date"
+                    />
+                  </Form.Group>
+                </Col>
+                <Col>
+                  <Form.Group controlId="endDate" bsSize="large">
+                    <Form.Label>Fecha de culminación:</Form.Label>
+                    <Form.Control
+                      defaultValue={
+                        endDate
+                          ? new Date(endDate).toISOString().substr(0, 10)
+                          : ""
+                      }
+                      disabled={this.state.edit ? true : false}
+                      type="date"
+                    />
+                  </Form.Group>
+                </Col>
+              </Row>
 
-        <Col className="center">
-          <Form
-            key={
-              this.props.location.state.serie
-                ? this.props.location.state.serie.id
-                : 0
-            }
-            style={{ width: "70%", alignItems: "center" }}
-            onSubmit={this.onFormSubmit}
-          >
-            <Row>
-              <Col>
-                <Form.Group style={{ width: "100%" }} controlId="name">
-                  <Form.Label>Nombre:</Form.Label>
-                  <Form.Control defaultValue={name} type="text" />
-                </Form.Group>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <Form.Group controlId="initDate" bsSize="large">
-                  <Form.Label>Fecha de incio:</Form.Label>
-                  <Form.Control
-                    defaultValue={
-                      initDate
-                        ? new Date(initDate).toISOString().substr(0, 10)
-                        : ""
-                    }
-                    disabled={this.state.edit ? true : false}
-                    type="date"
-                  />
-                </Form.Group>
-              </Col>
-              <Col>
-                <Form.Group controlId="endDate" bsSize="large">
-                  <Form.Label>Fecha de culminación:</Form.Label>
-                  <Form.Control
-                    defaultValue={
-                      endDate
-                        ? new Date(endDate).toISOString().substr(0, 10)
-                        : ""
-                    }
-                    disabled={this.state.edit ? true : false}
-                    type="date"
-                  />
-                </Form.Group>
-              </Col>
-            </Row>
-
-            <Row>
-              <Col>
-                <Form.Group controlId="caracter">
-                  <Form.Label>Carácter:</Form.Label>
-                  <Form.Control
-                    defaultValue={
-                      caracterSerie ? caracterSerie.caracter_Name : ""
-                    }
-                    as="select"
-                    custom
-                  >
-                    <option>{""}</option>
-                    {this.state.reaches.map((reach) => (
-                      <option
-                        value={reach.caracter_Name}
-                        id={reach.id}
-                        key={reach.id}
-                      >
-                        {reach.caracter_Name}
+              <Row>
+                <Col>
+                  <Form.Group controlId="caracter">
+                    <Form.Label>Carácter:</Form.Label>
+                    <Form.Control
+                      defaultValue={
+                        caracterSerie ? caracterSerie.caracter_Name : ""
+                      }
+                      as="select"
+                      custom
+                    >
+                      <option id={caracterSerie ? caracterSerie.id : ""}>
+                        {caracterSerie ? caracterSerie.caracter_Name : ""}
                       </option>
-                    ))}
-                  </Form.Control>
-                </Form.Group>
-              </Col>
-              <Col>
-                <Form.Group controlId="numberOfGames">
-                  <Form.Label>Número de juegos:</Form.Label>
-                  <Form.Control
-                    defaultValue={numberOfGames ? numberOfGames : ""}
-                    type="numeric"
-                    name="numberOfGames"
-                  />
-                </Form.Group>
-              </Col>
-              <Col>
-                <Form.Group controlId="numberOfTeams">
-                  <Form.Label>Número de equipos:</Form.Label>
-                  <Form.Control
-                    defaultValue={numberOfTeams ? numberOfTeams : ""}
-                    type="numeric"
-                    name="numberOfTeams"
-                  />
-                </Form.Group>
-              </Col>
+                      {this.state.reaches.map((reach) => (
+                        <option
+                          value={reach.caracter_Name}
+                          id={reach.id}
+                          key={reach.id}
+                          onChange={this.onChangeSelect}
+                        >
+                          {reach.caracter_Name}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Form.Group>
+                </Col>
+                <Col>
+                  <Form.Group controlId="numberOfGames">
+                    <Form.Label>Número de juegos:</Form.Label>
+                    <Form.Control
+                      defaultValue={numberOfGames ? numberOfGames : ""}
+                      type="numeric"
+                      name="numberOfGames"
+                    />
+                  </Form.Group>
+                </Col>
+                <Col>
+                  <Form.Group controlId="numberOfTeams">
+                    <Form.Label>Número de equipos:</Form.Label>
+                    <Form.Control
+                      defaultValue={numberOfTeams ? numberOfTeams : ""}
+                      type="numeric"
+                      name="numberOfTeams"
+                    />
+                  </Form.Group>
+                </Col>
+              </Row>
+
+              <Button
+                style={{ float: "right" }}
+                className="mt-3 ml-3"
+                variant="secondary"
+                type="reset"
+              >
+                Reiniciar
+              </Button>
+              <Button
+                style={{ float: "right" }}
+                // onClick={this.handleOnClickAccept}
+                className="mt-3 ml-3"
+                variant="primary"
+                type="submit"
+              >
+                Aceptar
+              </Button>
+            </Form>
+          </Col>
+
+          <Col md={3}>
+            <Row className="mb-3 align-items-center">
+              <Button
+                style={{ float: "right" }}
+                size="sm"
+                onClick={this.handleAddCaracter}
+                variant="primary"
+              >
+                Agregar un Carácter
+              </Button>
             </Row>
 
-            <Button
-              style={{ float: "right" }}
-              className="mt-3 ml-3"
-              variant="secondary"
-              type="reset"
-            >
-              Reiniciar
-            </Button>
-            <Button
-              style={{ float: "right" }}
-              // onClick={this.handleOnClickAccept}
-              className="mt-3 ml-3"
-              variant="primary"
-              type="submit"
-            >
-              Aceptar
-            </Button>
-          </Form>
-        </Col>
+            <Row>
+              {this.state.addCaracter && (
+                <Navbar fixed="right">
+                  <Nav.Item>
+                    <Form onSubmit={this.onFormSubmitCaracter}>
+                      <Form.Group controlId="name">
+                        <Form.Label>Nombre:</Form.Label>
+                        <Form.Control
+                          type="text"
+                          defaultValue={
+                            this.state.editDirector
+                              ? this.state.itemEdit.name
+                              : ""
+                          }
+                        />
+                      </Form.Group>
+                      <Button
+                        className="mr-2"
+                        style={{ float: "left" }}
+                        variant="primary"
+                        type="submit"
+                      >
+                        Aceptar
+                      </Button>
+                      <Button
+                        style={{ float: "right" }}
+                        onClick={this.handleCloseAdd}
+                        variant="secondary"
+                      >
+                        Cancelar
+                      </Button>
+                    </Form>
+                  </Nav.Item>
+                </Navbar>
+              )}
+            </Row>
+          </Col>
+        </Row>
       </Container>
     );
   }
